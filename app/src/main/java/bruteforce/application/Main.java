15118a8b0afe7adac57dfcb7c6119c4c105386c5
Ahmed Kidwai <ahmedkidwai@Ahmeds-MacBook-Air.local>
2019-06-05 16:39:47 -0500

    Main file for application layer created

---
+++ app/src/main/java/bruteforce/application/Main.java
@@ -0,0 +1,15 @@
+package bruteforce.srsys.application;
+
+import bruteforce.srsys.presentation.CLI;
+
+public class Main() {
+    
+    // WIP: stub database.
+    public static final String dbName = "STUBDB";
+
+    public static void main(Strng[] args) 
+    {   
+        // Runs until program has completed all tasks.
+        CLI.run();
+    }
+}
