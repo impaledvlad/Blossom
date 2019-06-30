6853e0c75f3f134c92df7aa69733c854c457d4a4
Sabit Rahman <rahmans7@myumanitoba.ca>
2019-06-30 16:04:26 -0500

    Initialize hsqldb

--- app/src/main/java/bruteforce/application/Main.java
+++ app/src/main/java/bruteforce/application/Main.java
@@ -2,7 +2,26 @@ package bruteforce.application;
 
 public class Main {
 
+    private static String dbName="blossom";
+
     public static void main(String[] args) {
 
+    }
+
+    public static void setDBPathName(final String name) {
+        try {
+            Class.forName("org.hsqldb.jdbcDriver").newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        dbName = name;
+    }
+
+    public static String getDBPathName() {
+        return dbName;
     }
 }
