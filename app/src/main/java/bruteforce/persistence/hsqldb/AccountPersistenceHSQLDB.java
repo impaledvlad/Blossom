090b3da63822c33804c2c6d05532c6469986b0e8
Frank Zhong <zhongy36@myumanitoba.ca>
2019-07-03 14:43:26 -0500

    User 29 - fixed db issues

--- app/src/main/java/bruteforce/persistence/hsqldb/AccountPersistenceHSQLDB.java
+++ app/src/main/java/bruteforce/persistence/hsqldb/AccountPersistenceHSQLDB.java
@@ -2,6 +2,7 @@ package bruteforce.persistence.hsqldb;
 
 import android.annotation.TargetApi;
 
+import java.io.Serializable;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
--- app/src/main/java/bruteforce/presentation/MainActivity.java
+++ app/src/main/java/bruteforce/presentation/MainActivity.java
@@ -1,5 +1,7 @@
 package bruteforce.presentation;
 
+import android.content.Context;
+import android.content.res.AssetManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -20,7 +22,13 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.Locale;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+
+import bruteforce.application.Main;
 import bruteforce.business.AccessAccount;
 import bruteforce.business.AccessTask;
 import bruteforce.business.StringConverter;
@@ -50,6 +58,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //set this MainActivity to work activity_main.xml
+        copyDatabaseToDevice();
 
         converter = new StringConverter();
         tasks = new AccessTask("username1");
@@ -178,5 +187,58 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private void copyDatabaseToDevice() {
+        final String DB_PATH = "db";
+
+        String[] assetNames;
+        Context context = getApplicationContext();
+        File dataDirectory = context.getDir(DB_PATH, Context.MODE_PRIVATE);
+        AssetManager assetManager = getAssets();
+
+        try {
+
+            assetNames = assetManager.list(DB_PATH);
+            for (int i = 0; i < assetNames.length; i++) {
+                assetNames[i] = DB_PATH + "/" + assetNames[i];
+            }
+
+            copyAssetsToDirectory(assetNames, dataDirectory);
+
+            Main.setDBPathName(dataDirectory.toString() + "/" + Main.getDBPathName());
+
+        } catch (final IOException ioe) {
+            Messages.warning(this, "Unable to access application data: " + ioe.getMessage());
+        }
+    }
+
+    public void copyAssetsToDirectory(String[] assets, File directory) throws IOException {
+        AssetManager assetManager = getAssets();
+
+        for (String asset : assets) {
+            String[] components = asset.split("/");
+            String copyPath = directory.toString() + "/" + components[components.length - 1];
+
+            char[] buffer = new char[1024];
+            int count;
+
+            File outFile = new File(copyPath);
+
+            if (!outFile.exists()) {
+                InputStreamReader in = new InputStreamReader(assetManager.open(asset));
+                FileWriter out = new FileWriter(outFile);
+
+                count = in.read(buffer);
+                while (count != -1) {
+                    out.write(buffer, 0, count);
+                    count = in.read(buffer);
+                }
+
+                out.close();
+                in.close();
+            }
+        }
+    }
+
 
 }
