c3d30f5c7fab1a6a36c255679cab8786410ac86c
John Manning <manningj@myumanitoba.ca>
2019-07-17 15:38:17 -0500

    Fixed Enum Smell

--- app/src/main/java/bruteforce/presentation/SummaryActivity.java
+++ app/src/main/java/bruteforce/presentation/SummaryActivity.java
@@ -12,8 +12,16 @@ import com.bruteforce.blossom.R;
 import bruteforce.business.AccessTask;
 import bruteforce.application.Services;
 
+
 public class SummaryActivity extends AppCompatActivity {
     AccessTask tasks;
+    /**
+     onCreate
+
+     Purpose: setup Summary UI. Calls the methods to set total, completed and incomplete tasks.
+     Parameters: Bundle savedInstanceState
+     Returns: none
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,12 +31,25 @@ public class SummaryActivity extends AppCompatActivity {
         setTextAllTasks();
         setTextCompleted();
     }
+    /**
+     setTextAllTasks
 
+     Purpose: sets the text view for total tests.
+     Parameters: none
+     Returns: none
+     */
     private void setTextAllTasks(){
         TextView all = findViewById(R.id.text_all);
         int total = tasks.getTaskList().size();
         all.setText(Integer.toString(total));
     }
+    /**
+     setTextCompleted
+
+     Purpose: set text view for both complete and incomplete tasks.
+     Parameters: none
+     Returns: none
+     */
     private void setTextCompleted(){
         TextView complete = findViewById(R.id.textview_completed);
         TextView incomplete = findViewById(R.id.textview_incomplete);
@@ -37,15 +58,16 @@ public class SummaryActivity extends AppCompatActivity {
 
         int completed_count = 0;
         int incomplete_count = 0;
-
+    //loop through all the tasks,
         for (int i = 0; i<total;i++){
+            //check if it's completed or not, then increment the appropriate count.
             if (tasks.getTaskList().get(i).getCompleted()) {
                 completed_count++;
             }else{
                     incomplete_count++;
                 }
         }
-
+        //set the text items
         incomplete.setText(Integer.toString(incomplete_count));
         complete.setText(Integer.toString(completed_count));
     }
